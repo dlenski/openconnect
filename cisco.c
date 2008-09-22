77375c54cde22e250b89db471bbb342733dc9e26
David Woodhouse <David.Woodhouse@intel.com>
2008-09-21 18:32:40 -0700

    get connect/response handling working

--- cisco.c
+++ cisco.c
@@ -10,6 +10,7 @@
 #include <sys/ioctl.h>
 #include <linux/if_tun.h>
 #include <openssl/ssl.h>
+#include <openssl/err.h>
 #include <sys/utsname.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -18,37 +19,19 @@
 #define _GNU_SOURCE
 #include <getopt.h>
 
-#if 0
-/* If the cookie expires, you can get another one by connecting with 
-   the certificate:
-
-openssl s_client -cert mycert.pem -connect vpnserver:443 -crlf
-GET /+webvpn+/index.html HTTP/1.1
-User-Agent: AnyConnect Linux 2.2.0133
-Host: localhost
-Accept: */*
-Accept-Encoding: identity
-X-Transcend-Version: 1
-
-#endif
+#include "anyconnect.h"
 
 /* The master-secret is generated randomly by the client. The server
    responds with a DTLS Session-ID. These are enough to 'resume' the DTLS
    session, bypassing all the initial setup of a normal DTLS connection.
    Or you can just send traffic over the HTTPS connection... */
 
-struct cstp_option {
-	const char *option;
-	const char *value;
-	struct cstp_option *next;
-};
-
 char *cookie;
 char *hostname;
 unsigned char dtls_secret[48];
 int mtu = 1406;
 int deflate;
-const char *useragent = "Cisco AnyConnect VPN Agent for Windows 2.2.0";
+const char *useragent = "Open AnyConnect VPN Agent v0.01";
 int verbose;
 
 /* Set up a tuntap device. */
@@ -74,6 +57,7 @@ int setup_tun(struct cstp_option *options)
 	   telling the kernel at all */
 	return tun_fd;
 }
+
 
 SSL *open_https(const char *host)
 {
@@ -126,8 +110,8 @@ SSL *open_https(const char *host)
 	SSL_set_bio(https_ssl, https_bio, https_bio);
 
 	if (SSL_connect(https_ssl) <= 0) {
-		BIO *err_bio = BIO_new_fp(stderr, BIO_NOCLOSE);
-		fprintf(stderr,  "SSL connection failure\n");
+		fprintf(stderr, "SSL connection failure\n");
+		ERR_print_errors_fp(stderr);
 		SSL_free(https_ssl);
 		SSL_CTX_free(https_ctx);
 		return NULL;
@@ -136,86 +120,124 @@ SSL *open_https(const char *host)
 	return https_ssl;
 }
 
+
+int  __attribute__ ((format (printf, 2, 3))) my_SSL_printf(SSL *ssl, const char *fmt, ...) 
+{
+	char buf[1024];
+	va_list args;
+
+	
+	buf[1023] = 0;
+
+	va_start(args, fmt);
+	vsnprintf(buf, 1023, fmt, args);
+	va_end(args);
+	if (verbose)
+		printf("%s", buf);
+	return SSL_write(ssl, buf, strlen(buf));
+
+}
+
+int my_SSL_gets(SSL *ssl, char *buf, size_t len)
+{
+	int i = 0;
+	int ret;
+
+	if (len < 2)
+		return -EINVAL;
+
+	while ( (ret = SSL_read(ssl, buf + i, 1)) == 1) {
+		if (buf[i] == '\n') {
+			buf[i] = 0;
+			if (i && buf[i-1] == '\r') {
+				buf[i-1] = 0;
+				i--;
+			}
+			return i;
+		}
+		i++;
+
+		if (i >= len - 1) {
+			buf[i] = 0;
+			return i;
+		}
+	}
+
+	buf[i] = 0;
+	return i?:ret;
+}
+
 struct cstp_option *start_ssl_connection(SSL *ssl)
 {
 	char buf[65536];
-	int i, state = 0;
-	struct cstp_option *options = NULL, **next_opt = &options;
+	int i;
 	struct utsname utsbuf;
-	BIO *bio = BIO_new(BIO_f_ssl());
-	
-	BIO_set_ssl(bio, ssl, BIO_NOCLOSE);
-	
+	struct cstp_option *options = NULL, **next_option = &options;
+
 	if (uname(&utsbuf))
-		sprintf(utsbuf.nodename, "localhost");
+		printf(utsbuf.nodename, "localhost");
 
 	if (verbose)
 		printf("Connected to HTTPS on %s\n", hostname);
 
-	BIO_printf(bio, "CONNECT /CSCOSSLC/tunnel HTTP/1.1\r\n");
-	BIO_printf(bio, "Host: %s\r\n", hostname);
-	BIO_printf(bio, "User-Agent: %S\r\n", useragent);
-	BIO_printf(bio, "Cookie: webvpn=%s\r\n", cookie);
-	BIO_printf(bio, "X-CSTP-Version: 1\r\n");
-	BIO_printf(bio, "X-CSTP-Hostname: %s\r\n", utsbuf.nodename);
+	my_SSL_printf(ssl, "CONNECT /CSCOSSLC/tunnel HTTP/1.1\r\n");
+	my_SSL_printf(ssl, "Host: %s\r\n", hostname);
+	my_SSL_printf(ssl, "User-Agent: %s\r\n", useragent);
+	my_SSL_printf(ssl, "Cookie: webvpn=%s\r\n", cookie);
+	my_SSL_printf(ssl, "X-CSTP-Version: 1\r\n");
+	my_SSL_printf(ssl, "X-CSTP-Hostname: %s\r\n", utsbuf.nodename);
 	if (deflate)
-		BIO_printf(bio, "X-CSTP-Accept-Encoding: deflate;q=1.0\r\n");
-	BIO_printf(bio, "X-CSTP-MTU: %d\r\n", mtu);
-	BIO_printf(bio, "X-CSTP-Address-Type: IPv6,IPv4\r\n");
-	BIO_printf(bio, "X-DTLS-Master-Secret: ");
+		my_SSL_printf(ssl, "X-CSTP-Accept-Encoding: deflate;q=1.0\r\n");
+	my_SSL_printf(ssl, "X-CSTP-MTU: %d\r\n", mtu);
+	my_SSL_printf(ssl, "X-CSTP-Address-Type: IPv6,IPv4\r\n");
+	my_SSL_printf(ssl, "X-DTLS-Master-Secret: ");
 	for (i = 0; i < sizeof(dtls_secret); i++)
-		BIO_printf(bio, "%02X", dtls_secret[i]);
-	BIO_printf(bio, "X-DTLS-CipherSuite: AES256-SHA:AES128-SHA:DES-CBC3-SHA:DES-CBC-SHA\r\n\r\n");
+		my_SSL_printf(ssl, "%02X", dtls_secret[i]);
+	my_SSL_printf(ssl, "\r\nX-DTLS-CipherSuite: AES256-SHA:AES128-SHA:DES-CBC3-SHA:DES-CBC-SHA\r\n\r\n");
 
-	state = 0;
-#if 0
-	state = BIO_gets(bio, buf, sizeof(buf)-1);
-
-	if (state <= 0) {
-		fprintf(stderr, "Error getting HTTP CONNECT response: %d\n", state);
-		BIO_free(bio);
+	if (my_SSL_gets(ssl, buf, 65536) < 0) {
+		fprintf(stderr, "Error fetching HTTPS response\n");
 		return NULL;
 	}
-	if (!strncmp(buf, "HTTP/1.1 200 ", 13)) {
+
+	if (verbose)
+		printf("Got CONNECT response: %s\n", buf);
+
+	if (strncmp(buf, "HTTP/1.1 200 ", 13)) {
 		fprintf(stderr, "Got inappropriate HTTP CONNECT response: %s\n",
 			buf);
-		BIO_free(bio);
 		return NULL;
 	}
 
-	while (BIO_gets(bio, buf, sizeof(buf)-1)) {
+
+	while ((i=my_SSL_gets(ssl, buf, sizeof(buf)))) {
 		char *colon = strchr(buf, ':');
+		if (!colon)
+			continue;
 
-		if (buf[strlen(buf)] == '\r') {
-			printf("ends \\r\n");
-			buf[strlen(buf)] = 0;
-		}
+		*colon = 0;
+		colon++;
+		if (*colon == ' ')
+			colon++;
 
-		if (!strlen(buf))
-			break;
-
-		fprintf(stderr, "Got: %s\n", buf);
+		*next_option = malloc(sizeof(*options));
+		(*next_option)->option = strdup(buf);
+		(*next_option)->value = strdup(colon);
+		(*next_option)->next = NULL;
+		next_option = &(*next_option)->next;
 	}
-#else
-        while (state < 4) {
-                SSL_read(ssl, buf, 1);
-                if ((state == 0 || state == 2) &&
-                    buf[0] == '\r')
-                        state++;
-                else if ((state == 1 || state == 3) &&
-                    buf[0] == '\n')
-                        state++;
-                else state = 0;
-                write(1, buf, 1);
-        }
-#endif
-	return NULL;
+
+	if (verbose)
+		printf("Connected!\n");
+	return options;
 }
 
 static struct option long_options[] = {
 	{"cookie", 1, 0, 'c'},
 	{"host", 1, 0, 'h'},
 	{"mtu", 1, 0, 'm'},
+	{"verbose", 1, 0, 'v'},
+	{"deflate", 1, 0, 'd'},
 };
 
 int main(int argc, char **argv)
@@ -232,7 +254,7 @@ int main(int argc, char **argv)
 	SSL_load_error_strings ();
 	OpenSSL_add_all_algorithms ();
 
-	while (opt = getopt_long(argc, argv, "c:h:", long_options, &optind)) {
+	while ((opt = getopt_long(argc, argv, "c:h:vd", long_options, &optind))) {
 		if (opt < 0)
 			break;
 
@@ -252,6 +274,15 @@ int main(int argc, char **argv)
 				exit(1);
 			}
 			break;
+
+		case 'v':
+			verbose = 1;
+			break;
+
+		case 'd':
+			fprintf(stderr, "Deflate not yet supported\n");
+			//deflate = 1;
+			break;
 		}
 	}
 	if (!hostname || !cookie) {
@@ -268,7 +299,12 @@ int main(int argc, char **argv)
 
 	opts = start_ssl_connection(https_ssl);
 
+	while (opts) {
+		printf ("Got opt %s, val %s\n", opts->option, opts->value);
+		opts = opts->next;
+	}
 	exit(1);
+
 	printf("Connected\n");
 
 	tun_fd = setup_tun(NULL);
