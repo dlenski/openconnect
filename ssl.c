fab6c13d42ae008164cf1665c12da38ab7e2befb
David Woodhouse <David.Woodhouse@intel.com>
2008-11-20 18:58:09 +0000

    Fix up licensing headers
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- ssl.c
+++ ssl.c
@@ -1,14 +1,15 @@
 /*
- * Open AnyConnect (SSL + DTLS) client
+ * OpenConnect (SSL + DTLS) VPN client
  *
- * © 2008 David Woodhouse <dwmw2@infradead.org>
+ * Copyright © 2008 Intel Corporation.
  *
- * This library is free software; you can redistribute it and/or
+ * Author: David Woodhouse <dwmw2@infradead.org>
+ *
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ * version 2.1, as published by the Free Software Foundation.
  *
- * This library is distributed in the hope that it will be useful, but
+ * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
