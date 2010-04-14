efcd1798d946bf1341e6d2c2f9a6f471ad31c1f6
David Woodhouse <David.Woodhouse@intel.com>
2010-04-14 14:13:17 +0100

    Move unhex() out of DTLS ifdef, to build with OpenSSL 0.9.7 again
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- dtls.c
+++ dtls.c
@@ -35,6 +35,19 @@
 
 #include "openconnect.h"
 
+static unsigned char nybble(unsigned char n)
+{
+	if      (n >= '0' && n <= '9') return n - '0';
+	else if (n >= 'A' && n <= 'F') return n - ('A' - 10);
+	else if (n >= 'a' && n <= 'f') return n - ('a' - 10);
+	return 0;
+}
+
+unsigned char unhex(const char *data)
+{
+	return (nybble(data[0]) << 4) | nybble(data[1]);
+}
+
 #ifdef SSL_F_DTLS1_CONNECT
 #if 0
 /*
@@ -89,19 +102,6 @@ int RAND_bytes(char *buf, int len)
  * my subsequent data packets. So we use the old protocol, which is what
  * their clients use anyway.
  */
-
-static unsigned char nybble(unsigned char n)
-{
-	if      (n >= '0' && n <= '9') return n - '0';
-	else if (n >= 'A' && n <= 'F') return n - ('A' - 10);
-	else if (n >= 'a' && n <= 'f') return n - ('a' - 10);
-	return 0;
-}
-
-unsigned char unhex(const char *data)
-{
-	return (nybble(data[0]) << 4) | nybble(data[1]);
-}
 
 int connect_dtls_socket(struct openconnect_info *vpninfo)
 {
