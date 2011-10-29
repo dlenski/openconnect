f6b3e8823556024336309bb2f0e952255ae0afb1
David Woodhouse <David.Woodhouse@intel.com>
2011-10-29 15:33:36 +0200

    Attempt to build without NLS if intltool isn't available
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-intltoolize --force --copy --automake && \
+intltoolize --force --copy --automake || \
+    echo "*** Continuing without NLS support..."
+
     aclocal && \
         libtoolize --automake --copy --force && \
             automake --foreign --add-missing && \
