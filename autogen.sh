b976ecb02aed356fe45334c018a0e727328a1c9a
David Woodhouse <David.Woodhouse@intel.com>
2011-11-03 00:59:00 +0000

    Revamp NLS support. Drop intltool, code po/Makefile.am manually
    Using $(shell ...) probably isn't portable, so may need to be expanded by
    the configure script. But this is a start...
    
    Using gettextize seemed to pull in a bunch of other crap; I'd rather just
    disable NLS support on crappy platforms where the msgfmt tool doesn't exist,
    or where dgettext() isn't even available in -lintl.
    
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-intltoolize --force --copy --automake || \
-    echo "*** Continuing without NLS support..."
-
     aclocal && \
         libtoolize --automake --copy --force && \
             automake --foreign --add-missing && \
