2d08bf0a90682b03254e5e73522ddc52ef0b11e5
David Woodhouse <David.Woodhouse@intel.com>
2012-06-18 20:06:36 +0100

    Fix PKCS#11 cleanup when no SSL certificate is set
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- gnutls.c
+++ gnutls.c
@@ -1515,8 +1515,8 @@ void openconnect_close_https(struct openconnect_info *vpninfo, int final)
 		gnutls_certificate_free_credentials(vpninfo->https_cred);
 		vpninfo->https_cred = NULL;
 #ifdef HAVE_P11KIT
-		if (!strncmp(vpninfo->cert, "pkcs11:", 7) ||
-		    !strncmp(vpninfo->sslkey, "pkcs11:", 7)) {
+		if ((vpninfo->cert && !strncmp(vpninfo->cert, "pkcs11:", 7)) ||
+		    (vpninfo->sslkey && !strncmp(vpninfo->sslkey, "pkcs11:", 7))) {
 			char pin_source[40];
 
 			sprintf(pin_source, "openconnect:%p", vpninfo);
