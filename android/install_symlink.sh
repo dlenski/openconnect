a65da8a444fa3a1592f865d4dd1c7e5c0f8fb789
David Woodhouse <David.Woodhouse@intel.com>
2013-03-06 11:12:44 +0000

    Revamp Android build infrastructure
    The existing setup would only build as part of a full AOSP build, not as
    a standalone application with the NDK. Fix that...
    
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

---
+++ android/install_symlink.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+unset SRCS
+unset DST
+
+while [ $# -gt 1 ]; do
+    case "$1" in
+	-d)
+	    MAKEDIR=1
+	    shift
+	    ;;
+	-c|-C|-s)
+	    shift
+	    ;;
+	-m|-g|-o)
+	    shift 2;
+	    ;;
+	*)
+	    SRCS="$SRCS $(readlink -f "$1")"
+	    shift
+	    ;;
+    esac
+done
+if [ ! -z $MAKEDIR ]; then
+    mkdir -p $1
+fi
+ln -sf $SRCS "$1"
+
