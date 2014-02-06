6bf5b0509356a38287a148eb9513a414439b5fad
David Woodhouse <David.Woodhouse@intel.com>
2014-02-06 20:02:52 +0000

    Remove FSF address from copyright boilerplate
    Intel guidelines now recommend that this is omitted.
    
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- main.c
+++ main.c
@@ -15,13 +15,6 @@
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to:
- *
- *   Free Software Foundation, Inc.
- *   51 Franklin Street, Fifth Floor,
- *   Boston, MA 02110-1301 USA
  */
 
 #ifdef HAVE_GETLINE
