dlenski/openconnect

commit c7077b96b6dbf54781321bf9e811182960d11870

Do not abort when GnuTLS reports unclean shutdown
Some servers are observed to send an HTTP 1.0 response and then just kill
the socket. This is horrid, but we need to cope. If we get the
GNUTLS_E_PREMATURE_TERMINATION "error" when trying to read an empty
HTTP body, we shouldn't abort the connection attempt. We should just
treat it as EOF.

https://bugs.launchpad.net/bugs/1225276

Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

gnutls.c
@@ -116,6 +116,12 @@ int openconnect_SSL_read(struct openconnect_info *vpninfo, char *buf, size_t len
 				vpn_progress(vpninfo, PRG_ERR, _("SSL read cancelled\n"));
 				return -EINTR;
 			}
+		} else if (done == GNUTLS_E_PREMATURE_TERMINATION) {
+			/* We've seen this with HTTP 1.0 responses followed by abrupt
+			   socket closure and no clean SSL shutdown.
+			   https://bugs.launchpad.net/bugs/1225276 */
+			vpn_progress(vpninfo, PRG_TRACE, _("SSL socket closed uncleanly\n"));
+			return 0;
 		} else {
 			vpn_progress(vpninfo, PRG_ERR, _("Failed to read from SSL socket: %s\n"),
 				     gnutls_strerror(done));

commit 0eda57f2d3a6387c6b20ba66068cce22894f5690

Fix GnuTLS 2.x build
Commit c7077b96b ("Do not abort when GnuTLS reports unclean shutdown")
introduced a build failure for GnuTLS 2.x which lacks the
GNUTLS_E_PREMATURE_TERMINATION error that we check for. Instead, it
returns GNUTLS_E_UNEXPECTED_PACKET_LENGTH.

Since the offending server turned out not to be an AnyConnect server at
all in the end, gracefully handling the unclean SSL shutdown probably
isn't strictly necessary — although it remains a good idea on general
principles. But it's not *so* important that I'm going to jump through
hoops to do it even for users who are still using GnuTLS 2.x.

So just put the check inside #ifdef GNUTLS_E_PREMATURE_TERMINATION, and
if we later find an *AnyConnect* server which displays this behaviour and
requires us to cope with it, we can find a better solution. Which might
just be to deprecate GnuTLS 2.x entirely... ☺

Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

gnutls.c
@@ -116,12 +116,14 @@ int openconnect_SSL_read(struct openconnect_info *vpninfo, char *buf, size_t len
 				vpn_progress(vpninfo, PRG_ERR, _("SSL read cancelled\n"));
 				return -EINTR;
 			}
+#ifdef GNUTLS_E_PERMATURE_TERMINATION
 		} else if (done == GNUTLS_E_PREMATURE_TERMINATION) {
 			/* We've seen this with HTTP 1.0 responses followed by abrupt
 			   socket closure and no clean SSL shutdown.
 			   https://bugs.launchpad.net/bugs/1225276 */
 			vpn_progress(vpninfo, PRG_TRACE, _("SSL socket closed uncleanly\n"));
 			return 0;
+#endif
 		} else {
 			vpn_progress(vpninfo, PRG_ERR, _("Failed to read from SSL socket: %s\n"),
 				     gnutls_strerror(done));