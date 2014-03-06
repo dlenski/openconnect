210370997a9ae3f0a30d3cbccf3ec017e181db71
Jay Soffian <jaysoffian@gmail.com>
2014-03-06 14:04:34 -0500

    Allow libtoolize to be specified via environment variable
    On OS X, libtoolize is known as glibtoolize. Allow this to be
    specified via an environment variable so that autogen.sh need
    not be modified to build.
    
    Signed-off-by: Jay Soffian <jaysoffian@gmail.com>
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
     aclocal && \
-        libtoolize --automake --force && \
+        ${LIBTOOLIZE:-libtoolize} --automake --force && \
             automake --foreign --add-missing --force && \
                 autoconf --force
