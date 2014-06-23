1cdefb010f14bfbe7bd34c521b39fd25d5fedad2
David Woodhouse <David.Woodhouse@intel.com>
2014-06-23 20:35:27 +0100

    Accept 'GSSAPI' in place of 'Negotiate' in --proxy-auth=
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- http.c
+++ http.c
@@ -1995,7 +1995,9 @@ int openconnect_set_proxy_auth(struct openconnect_info *vpninfo, char *methods)
 			*(p++) = 0;
 
 		for (i = 0; i < sizeof(auth_methods) / sizeof(auth_methods[0]); i++) {
-			if (!strcasecmp(methods, auth_methods[i].name)) {
+			if (!strcasecmp(methods, auth_methods[i].name) ||
+			    (auth_methods[i].state_index == AUTH_TYPE_GSSAPI &&
+			     !strcasecmp(methods, "gssapi"))) {
 				vpninfo->auth[auth_methods[i].state_index].state = AUTH_UNSEEN;
 				break;
 			}
