4b701f473aea4856d94fd0bc9546acae1570c1ac
David Woodhouse <David.Woodhouse@intel.com>
2014-06-27 09:33:43 +0100

    Fix untranslated error message
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- gnutls.c
+++ gnutls.c
@@ -1834,7 +1834,8 @@ static int verify_peer(gnutls_session_t session)
 	}
  done:
 	if (reason) {
-		vpn_progress(vpninfo, PRG_INFO, "Server certificate verify failed: %s\n",
+		vpn_progress(vpninfo, PRG_INFO,
+			     _("Server certificate verify failed: %s\n"),
 			     reason);
 		if (vpninfo->validate_peer_cert)
 			err = vpninfo->validate_peer_cert(vpninfo->cbdata,
