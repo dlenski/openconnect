a10d4d98620185e9633608547d0debea099b76b7
Kevin Cernekee <cernekee@gmail.com>
2014-08-02 14:36:39 -0700

    auth: Rearrange stoken support code
    Put everything under one giant #ifdef to make it easier to add helper
    functions later.  No change to the program logic.
    
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

--- auth.c
+++ auth.c
@@ -1024,6 +1024,8 @@ bad:
 	return -ENOMEM;
 }
 
+#ifdef HAVE_LIBSTOKEN
+
 /*
  * If the user clicks OK without entering any data, we will continue
  * connecting but bypass soft token generation for the duration of
@@ -1038,7 +1040,6 @@ bad:
  */
 int prepare_stoken(struct openconnect_info *vpninfo)
 {
-#ifdef HAVE_LIBSTOKEN
 	struct oc_auth_form form;
 	struct oc_form_opt opts[3], *opt = opts;
 	char **devid = NULL, **pass = NULL, **pin = NULL;
@@ -1146,9 +1147,6 @@ int prepare_stoken(struct openconnect_info *vpninfo)
 
 	nuke_opt_values(opts);
 	return ret;
-#else
-	return -EOPNOTSUPP;
-#endif
 }
 
 /* Return value:
@@ -1159,7 +1157,6 @@ static int can_gen_stoken_code(struct openconnect_info *vpninfo,
 			       struct oc_auth_form *form,
 			       struct oc_form_opt *opt)
 {
-#ifdef HAVE_LIBSTOKEN
 	if ((strcmp(opt->name, "password") && strcmp(opt->name, "answer")) ||
 	    vpninfo->token_bypassed)
 		return -EINVAL;
@@ -1179,10 +1176,52 @@ static int can_gen_stoken_code(struct openconnect_info *vpninfo,
 		return -ENOENT;
 	}
 	return 0;
-#else
-	return -EOPNOTSUPP;
-#endif
 }
+
+static int do_gen_stoken_code(struct openconnect_info *vpninfo,
+			      struct oc_auth_form *form,
+			      struct oc_form_opt *opt)
+{
+	char tokencode[STOKEN_MAX_TOKENCODE + 1];
+
+	if (!vpninfo->token_time)
+		vpninfo->token_time = time(NULL);
+	vpn_progress(vpninfo, PRG_INFO, _("Generating RSA token code\n"));
+
+	/* This doesn't normally fail */
+	if (stoken_compute_tokencode(vpninfo->stoken_ctx, vpninfo->token_time,
+				     vpninfo->stoken_pin, tokencode) < 0) {
+		vpn_progress(vpninfo, PRG_ERR, _("General failure in libstoken.\n"));
+		return -EIO;
+	}
+
+	vpninfo->token_tries++;
+	opt->value = strdup(tokencode);
+	return opt->value ? 0 : -ENOMEM;
+}
+
+#else
+
+int prepare_stoken(struct openconnect_info *vpninfo)
+{
+	return -EOPNOTSUPP;
+}
+
+static int can_gen_stoken_code(struct openconnect_info *vpninfo,
+			       struct oc_auth_form *form,
+			       struct oc_form_opt *opt)
+{
+	return -EOPNOTSUPP;
+}
+
+static int do_gen_stoken_code(struct openconnect_info *vpninfo,
+			      struct oc_auth_form *form,
+			      struct oc_form_opt *opt)
+{
+	return 0;
+}
+
+#endif
 
 /* Return value:
  *  < 0, if unable to generate a tokencode
@@ -1268,32 +1307,6 @@ static int can_gen_tokencode(struct openconnect_info *vpninfo,
 	default:
 		return -EINVAL;
 	}
-}
-
-static int do_gen_stoken_code(struct openconnect_info *vpninfo,
-			      struct oc_auth_form *form,
-			      struct oc_form_opt *opt)
-{
-#ifdef HAVE_LIBSTOKEN
-	char tokencode[STOKEN_MAX_TOKENCODE + 1];
-
-	if (!vpninfo->token_time)
-		vpninfo->token_time = time(NULL);
-	vpn_progress(vpninfo, PRG_INFO, _("Generating RSA token code\n"));
-
-	/* This doesn't normally fail */
-	if (stoken_compute_tokencode(vpninfo->stoken_ctx, vpninfo->token_time,
-				     vpninfo->stoken_pin, tokencode) < 0) {
-		vpn_progress(vpninfo, PRG_ERR, _("General failure in libstoken.\n"));
-		return -EIO;
-	}
-
-	vpninfo->token_tries++;
-	opt->value = strdup(tokencode);
-	return opt->value ? 0 : -ENOMEM;
-#else
-	return 0;
-#endif
 }
 
 static int do_gen_totp_code(struct openconnect_info *vpninfo,
