dlenski/openconnect

commit f8fbc0b30d5789b2e1168f47c768a8c004a1af54

Add vpn_perror() macro and use it instead of perror()
Might be slightly nicer to just change all instances to open-code it and
use vpn_progress(), but then I'd need to change all the translated strings
too.

Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

tun.c
@@ -94,7 +94,7 @@ static int set_tun_mtu(struct openconnect_info *vpninfo)
 
 	net_fd = socket(PF_INET, SOCK_DGRAM, 0);
 	if (net_fd < 0) {
-		perror(_("open net"));
+		vpn_perror(vpninfo, _("open net"));
 		return -EINVAL;
 	}
 
@@ -103,7 +103,7 @@ static int set_tun_mtu(struct openconnect_info *vpninfo)
 	ifr.ifr_mtu = vpninfo->ip_info.mtu;
 
 	if (ioctl(net_fd, SIOCSIFMTU, &ifr) < 0)
-		perror(_("SIOCSIFMTU"));
+		vpn_perror(vpninfo, _("SIOCSIFMTU"));
 
 	close(net_fd);
 #endif
@@ -118,11 +118,11 @@ static int link_proto(int unit_nr, const char *devname, uint64_t flags)
 
 	tun2_fd = open("/dev/tun", O_RDWR);
 	if (tun2_fd < 0) {
-		perror(_("Could not open /dev/tun for plumbing"));
+		vpn_perror(vpninfo, _("Could not open /dev/tun for plumbing"));
 		return -EIO;
 	}
 	if (ioctl(tun2_fd, I_PUSH, "ip") < 0) {
-		perror(_("Can't push IP"));
+		vpn_perror(vpninfo, _("Can't push IP"));
 		close(tun2_fd);
 		return -EIO;
 	}
@@ -132,7 +132,7 @@ static int link_proto(int unit_nr, const char *devname, uint64_t flags)
 	ifr.lifr_flags = flags;
 
 	if (ioctl(tun2_fd, SIOCSLIFNAME, &ifr) < 0) {
-		perror(_("Can't set ifname"));
+		vpn_perror(vpninfo, _("Can't set ifname"));
 		close(tun2_fd);
 		return -1;
 	}
@@ -232,19 +232,19 @@ intptr_t os_setup_tun(struct openconnect_info *vpninfo)
 
 	tun_fd = open("/dev/tun", O_RDWR);
 	if (tun_fd < 0) {
-		perror(_("open /dev/tun"));
+		vpn_perror(vpninfo, _("open /dev/tun"));
 		return -EIO;
 	}
 
 	unit_nr = ioctl(tun_fd, TUNNEWPPA, -1);
 	if (unit_nr < 0) {
-		perror(_("Failed to create new tun"));
+		vpn_perror(vpninfo, _("Failed to create new tun"));
 		close(tun_fd);
 		return -EIO;
 	}
 
 	if (ioctl(tun_fd, I_SRDOPT, RMSGD) < 0) {
-		perror(_("Failed to put tun file descriptor into message-discard mode"));
+		vpn_perror(vpninfo, _("Failed to put tun file descriptor into message-discard mode"));
 		close(tun_fd);
 		return -EIO;
 	}
@@ -408,7 +408,7 @@ intptr_t os_setup_tun(struct openconnect_info *vpninfo)
 #ifdef TUNSIFHEAD
 	i = 1;
 	if (ioctl(tun_fd, TUNSIFHEAD, &i) < 0) {
-		perror(_("TUNSIFHEAD"));
+		vpn_perror(vpninfo, _("TUNSIFHEAD"));
 		close(tun_fd);
 		return -EIO;
 	}
@@ -458,11 +458,11 @@ int openconnect_setup_tun_script(struct openconnect_info *vpninfo,
 		return -EIO;
 	} else if (!child) {
 		if (setpgid(0, getpid()) < 0)
-			perror(_("setpgid"));
+			vpn_perror(vpninfo, _("setpgid"));
 		close(fds[0]);
 		setenv_int("VPNFD", fds[1]);
 		execl("/bin/sh", "/bin/sh", "-c", vpninfo->vpnc_script, NULL);
-		perror(_("execl"));
+		vpn_perror(vpninfo, _("execl"));
 		exit(1);
 	}
 	close(fds[1]);

dtls.c
@@ -499,7 +499,7 @@ int connect_dtls_socket(struct openconnect_info *vpninfo)
 
 	dtls_fd = socket(vpninfo->peer_addr->sa_family, SOCK_DGRAM, IPPROTO_UDP);
 	if (dtls_fd < 0) {
-		perror(_("Open UDP socket for DTLS:"));
+		vpn_perror(vpninfo, _("Open UDP socket for DTLS:"));
 		return -EINVAL;
 	}
 	if (vpninfo->protect_socket)
@@ -538,14 +538,14 @@ int connect_dtls_socket(struct openconnect_info *vpninfo)
 		}
 
 		if (bind(dtls_fd, (struct sockaddr *)&dtls_bind_addr, dtls_bind_addrlen)) {
-			perror(_("Bind UDP socket for DTLS"));
+			vpn_perror(vpninfo, _("Bind UDP socket for DTLS"));
 			closesocket(dtls_fd);
 			return -EINVAL;
 		}
 	}
 
 	if (connect(dtls_fd, vpninfo->dtls_addr, vpninfo->peer_addrlen)) {
-		perror(_("UDP (DTLS) connect:\n"));
+		vpn_perror(vpninfo, _("UDP (DTLS) connect:\n"));
 		closesocket(dtls_fd);
 		return -EINVAL;
 	}

openconnect-internal.h
@@ -464,6 +464,7 @@ struct openconnect_info {
 #define AC_PKT_TERM_SERVER	9	/* Server kick */
 
 #define vpn_progress(vpninfo, ...) (vpninfo)->progress((vpninfo)->cbdata, __VA_ARGS__)
+#define vpn_perror(vpninfo, msg) vpn_progress((vpninfo), PRG_ERR, "%s: %s", (msg), strerror(errno));
 
 /****************************************************************************/
 /* Oh Solaris how we hate thee! */

commit 2fe491525c15bc89ee78ca3c6eba4033dcf035ab

Fix Solaris build
Broken by commit f8fbc0b3 ("Add vpn_perror() macro and use it instead of
perror()") which makes link_proto() use a 'vpninfo' variable it doesn't
have.

Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

tun.c
@@ -111,7 +111,8 @@ static int set_tun_mtu(struct openconnect_info *vpninfo)
 }
 
 #ifdef __sun__
-static int link_proto(int unit_nr, const char *devname, uint64_t flags)
+static int link_proto(struct openconnect_info *vpninfo, int unit_nr,
+		      const char *devname, uint64_t flags)
 {
 	int ip_fd, mux_id, tun2_fd;
 	struct lifreq ifr;
@@ -252,14 +253,14 @@ intptr_t os_setup_tun(struct openconnect_info *vpninfo)
 	sprintf(tun_name, "tun%d", unit_nr);
 	vpninfo->ifname = strdup(tun_name);
 
-	vpninfo->ip_fd = link_proto(unit_nr, "/dev/udp", IFF_IPV4);
+	vpninfo->ip_fd = link_proto(vpninfo, unit_nr, "/dev/udp", IFF_IPV4);
 	if (vpninfo->ip_fd < 0) {
 		close(tun_fd);
 		return -EIO;
 	}
 
 	if (vpninfo->ip_info.addr6) {
-		vpninfo->ip6_fd = link_proto(unit_nr, "/dev/udp6", IFF_IPV6);
+		vpninfo->ip6_fd = link_proto(vpninfo, unit_nr, "/dev/udp6", IFF_IPV6);
 		if (vpninfo->ip6_fd < 0) {
 			close(tun_fd);
 			close(vpninfo->ip_fd);