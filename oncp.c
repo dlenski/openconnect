00e4586d7774aed99535126e469fe9f884169eca
David Woodhouse <David.Woodhouse@intel.com>
2015-01-16 17:21:39 -0800

    First negotiation packets for oNCP
    Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

--- oncp.c
+++ oncp.c
@@ -36,9 +36,6 @@ void oncp_common_headers(struct openconnect_info *vpninfo, struct oc_text_buf *b
 	struct oc_vpn_option *opt;
 
 	buf_append(buf, "Host: %s\r\n", vpninfo->hostname);
-	buf_append(buf, "User-Agent: %s\r\n", vpninfo->useragent);
-	buf_append(buf, "Accept: */*\r\n");
-	buf_append(buf, "Accept-Encoding: identity\r\n");
 
 	if (vpninfo->cookies) {
 		buf_append(buf, "Cookie: ");
@@ -46,6 +43,11 @@ void oncp_common_headers(struct openconnect_info *vpninfo, struct oc_text_buf *b
 			buf_append(buf, "%s=%s%s", opt->option,
 				      opt->value, opt->next ? "; " : "\r\n");
 	}
+
+	buf_append(buf, "Connection: close\r\n");
+//	buf_append(buf, "Content-Length: 256\r\n");
+	buf_append(buf, "NCP-Version: 3\r\n");
+//	buf_append(buf, "Accept-Encoding: gzip\r\n");
 }
 
 int oncp_obtain_cookie(struct openconnect_info *vpninfo)
@@ -53,9 +55,128 @@ int oncp_obtain_cookie(struct openconnect_info *vpninfo)
 	vpn_progress(vpninfo, PRG_ERR, _("oNCP authentication not yet implemented\n"));
 	return -EOPNOTSUPP;
 }
+static int parse_cookie(struct openconnect_info *vpninfo)
+{
+	char *p = vpninfo->cookie;
+
+	/* We currenly expect the "cookie" to be contain multiple cookies:
+	 * DSSignInUrl=/; DSID=xxx; DSFirstAccess=xxx; DSLastAccess=xxx
+	 * Process those into vpninfo->cookies unless we already had them
+	 * (in which case they'll may be newer. */
+	while (p && *p) {
+		char *semicolon = strchr(p, ';');
+		char *equals;
+
+		if (semicolon)
+			*semicolon = 0;
+
+		equals = strchr(p, '=');
+		if (!equals) {
+			vpn_progress(vpninfo, PRG_ERR, _("Invalid cookie '%s'\n"), p);
+			return -EINVAL;
+		}
+		*equals = 0;
+		http_add_cookie(vpninfo, p, equals+1, 0);
+		*equals = '=';
+
+		p = semicolon;
+		if (p) {
+			*p = ';';
+			p++;
+			while (*p && isspace(*p))
+				p++;
+		}
+	}
+
+	return 0;
+}
 
 int oncp_connect(struct openconnect_info *vpninfo)
 {
+	int ret;
+	struct oc_text_buf *reqbuf;
+	unsigned char bytes[1024];
+
+	if (!vpninfo->cookies) {
+		ret = parse_cookie(vpninfo);
+		if (ret)
+			return ret;
+	}
+
+	ret = openconnect_open_https(vpninfo);
+	if (ret)
+		return ret;
+	reqbuf = buf_alloc();
+ 	buf_append(reqbuf, "POST /dana/js?prot=1&svc=1 HTTP/1.1\r\n");
+	oncp_common_headers(vpninfo, reqbuf);
+	buf_append(reqbuf, "\r\n");
+
+	if (buf_error(reqbuf)) {
+		vpn_progress(vpninfo, PRG_ERR,
+			     _("Error creating oNCP negotiation request\n"));
+		return buf_free(reqbuf);
+	}
+
+	ret = vpninfo->ssl_write(vpninfo, reqbuf->data, reqbuf->pos);
+	if (ret < 0) {
+		buf_free(reqbuf);
+		return ret;
+	}
+	ret = process_http_response(vpninfo, 0, NULL, reqbuf);
+	if (ret < 0) {
+		/* We'll already have complained about whatever offended us */
+		buf_free(reqbuf);
+		return ret;
+	}
+	if (ret != 200) {
+		vpn_progress(vpninfo, PRG_ERR,
+			     _("Unexpected %d result from server\n"),
+			     ret);
+		buf_free(reqbuf);
+		return -EINVAL;
+	}
+
+	/* Now the second request. We should reduce the duplication
+	   here but let's not overthink it for now; we should see what
+	   the authentication requests are going to look like, and make
+	   do_https_request() or a new helper function work for those
+	   too. */
+	ret = openconnect_open_https(vpninfo);
+	if (ret) {
+		buf_free(reqbuf);
+		return ret;
+	}
+	buf_truncate(reqbuf);
+	buf_append(reqbuf, "POST /dana/js?prot=1&svc=4 HTTP/1.1\r\n");
+	oncp_common_headers(vpninfo, reqbuf);
+	buf_append(reqbuf, "\r\n");
+
+	if (buf_error(reqbuf)) {
+		vpn_progress(vpninfo, PRG_ERR,
+			     _("Error creating oNCP negotiation request\n"));
+		return buf_free(reqbuf);
+	}
+
+	ret = vpninfo->ssl_write(vpninfo, reqbuf->data, reqbuf->pos);
+	if (ret < 0) {
+		buf_free(reqbuf);
+		return ret;
+	}
+	ret = process_http_response(vpninfo, 1, NULL, reqbuf);
+	if (ret < 0) {
+		/* We'll already have complained about whatever offended us */
+		buf_free(reqbuf);
+		return ret;
+	}
+	if (ret != 200) {
+		vpn_progress(vpninfo, PRG_ERR,
+			     _("Unexpected %d result from server\n"),
+			     ret);
+		buf_free(reqbuf);
+		return -EINVAL;
+	}
+
+	buf_truncate(reqbuf);
 	return 0;
 }
 
