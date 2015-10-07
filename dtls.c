dlenski/openconnect

commit 287f535af3b3012ab79b9144fe6504afbd7075ce

Print the compression algorithm name after DTLS is connected
Signed-off-by: Nikos Mavrogiannopoulos <nmav@redhat.com>
Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

dtls.c
@@ -338,6 +338,12 @@ static int dtls_try_handshake(struct openconnect_info *vpninfo)
 			     _("Established DTLS connection (using OpenSSL). Ciphersuite %s.\n"),
 			     vpninfo->dtls_cipher);
 
+		c = openconnect_get_dtls_compression(vpninfo);
+		if (c) {
+			vpn_progress(vpninfo, PRG_INFO,
+				     _("DTLS connection compression using %s.\n"), c);
+		}
+
 		vpninfo->dtls_times.last_rekey = vpninfo->dtls_times.last_rx = 
 			vpninfo->dtls_times.last_tx = time(NULL);
 
@@ -625,10 +631,16 @@ static int dtls_try_handshake(struct openconnect_info *vpninfo)
 		vpninfo->dtls_state = DTLS_CONNECTED;
 		str = get_gnutls_cipher(vpninfo->dtls_ssl);
 		if (str) {
+			const char *c;
 			vpn_progress(vpninfo, PRG_INFO,
 				     _("Established DTLS connection (using GnuTLS). Ciphersuite %s.\n"),
 				     str);
 			gnutls_free(str);
+			c = openconnect_get_dtls_compression(vpninfo);
+			if (c) {
+				vpn_progress(vpninfo, PRG_INFO,
+					     _("DTLS connection compression using %s.\n"), c);
+			}
 		}
 
 		vpninfo->dtls_times.last_rekey = vpninfo->dtls_times.last_rx = 

commit 8984a8c066088fb6884387fc4a4af6b6ba44f373

Fix DTLS/OpenSSL build break
Commit 287f535af3 ("Print the compression algorithm name after DTLS is
connected") broke the OpenSSL build. Reported by Kamil Skowron.

Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

dtls.c
@@ -333,6 +333,7 @@ static int dtls_try_handshake(struct openconnect_info *vpninfo)
 	int ret = SSL_do_handshake(vpninfo->dtls_ssl);
 
 	if (ret == 1) {
+		const char *c;
 		vpninfo->dtls_state = DTLS_CONNECTED;
 		vpn_progress(vpninfo, PRG_INFO,
 			     _("Established DTLS connection (using OpenSSL). Ciphersuite %s.\n"),