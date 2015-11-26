dlenski/openconnect

commit a5dd38ec8549d28902ec4a2f7a57fa6c395de4e6

Assign Address-IP6 field to netmask instead of address
The Address-IP6 field combines the address and netmask into one line,
meaning it only makes sense in the netmask field.

Fixes an error rather not dissimilar to the following:

  Error: an inet prefix is expected rather than "<foo>/64/128"

Signed-off-by: Jeremy Visser <jeremy@visser.name>
Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

cstp.c
@@ -379,7 +379,7 @@ static int start_cstp_connection(struct openconnect_info *vpninfo)
 			if (cstpmtu > mtu)
 				mtu = cstpmtu;
 		} else if (!strcmp(buf + 7, "Address-IP6")) {
-			vpninfo->ip_info.addr6 = new_option->value;
+			vpninfo->ip_info.netmask6 = new_option->value;
 		} else if (!strcmp(buf + 7, "Address")) {
 			if (strchr(new_option->value, ':')) {
 				if (!vpninfo->disable_ipv6)

commit 430f8bcab5c0e10881f7dcdc07a15803ebf31607

Fix IPv6-only connectivity
Commit a5dd38ec8 ("Assign Address-IP6 field to netmask instead of address")
broke IPv6-only configurations, because we only check for ip_info.addr
and ip_info.addr6 being NULL. We need to allow the connection to continue
when ip_info.netmask6 is non-NULL too.

Reported-by: Dennis Gilmore <dennis@ausil.us>
Signed-off-by: David Woodhouse <David.Woodhouse@intel.com>

cstp.c
@@ -494,7 +494,8 @@ static int start_cstp_connection(struct openconnect_info *vpninfo)
 	}
 	vpninfo->ip_info.mtu = mtu;
 
-	if (!vpninfo->ip_info.addr && !vpninfo->ip_info.addr6) {
+	if (!vpninfo->ip_info.addr && !vpninfo->ip_info.addr6 &&
+	    !vpninfo->ip_info.netmask6) {
 		vpn_progress(vpninfo, PRG_ERR,
 			     _("No IP address received. Aborting\n"));
 		return -EINVAL;