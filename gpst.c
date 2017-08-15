1070bad755fc78ebc2410a589b4f954a377a1077
David Woodhouse <dwmw2@infradead.org>
2017-08-15 12:52:55 +0100

    Add copyright to GPST files
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- gpst.c
+++ gpst.c
@@ -1,6 +1,8 @@
 /*
  * OpenConnect (SSL + DTLS) VPN client
  *
+ * Copyright © 2016-2017 Daniel Lenski
+ *
  * Author: Daniel Lenski <dlenski@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
