4104ab742ebfe46f73a71a4841ad86006654a926
Daniel Lenski <dlenski@gmail.com>
2017-10-31 22:24:03 -0700

    add --no-verify, and use absence/presence of --md5 as a sign we need to check before submitting

--- hipreport.py
+++ hipreport.py
@@ -9,7 +9,8 @@ from sys import stderr, exit
 p = argparse.ArgumentParser()
 #p.add_argument('-v','--verbose', default=0, action='count')
 #g = p.add_argument_group('Login credentials')
-p.add_argument('--check', action='store_true', help='Check if HIP report is needed before submitting it.')
+p.add_argument('--no-verify', action='store_true', dest='verify', default=True, help="Don't verify server certificates.")
+p.add_argument('-m','--md5', help='Check if HIP report is needed by submitting MD5 digest last HIP file.')
 p.add_argument('-c','--cookie', required=True, help='Cookie value(32 characters).')
 p.add_argument('-u','--user', required=True, help='User.')
 p.add_argument('-i','--ip', required=True, help='IP.')
@@ -18,7 +19,6 @@ p.add_argument('-d','--domain', required=True, help='Domain.')
 p.add_argument('-p','--portal', required=True, help='Portal name.')
 p.add_argument('-g','--gateway', required=True, help='Gateway.')
 p.add_argument('-H','--hip', type=argparse.FileType('rb'), required=True, help='HIP file.')
-p.add_argument('-m','--md5', required=True, help='MD5 digest of HIP file.')
 args = p.parse_args()
 
 s = requests.Session()
@@ -31,14 +31,14 @@ data = {
     'authcookie' : args.cookie,
     'client-ip' : args.ip,
     'computer' : args.hostname,
-    'md5' : args.md5,
     'client-role' : 'global-protect-full',
+    'md5' : args.md5,
 }
 
-if not args.check:
+if not args.md5:
     needed = True
 else:
-    r = s.post('https://%s/ssl-vpn/hipreportcheck.esp' % args.gateway, data=data)
+    r = s.post('https://%s/ssl-vpn/hipreportcheck.esp' % args.gateway, data=data, verify=args.verify)
     if 'success' in r.text and '<hip-report-needed>no</hip-report-needed>' in r.text:
         print("No HIP report needed.", file=stderr)
         needed = False
@@ -54,7 +54,8 @@ if needed:
     with args.hip:
         report = args.hip.read()
     data['report'] = report
-    r = s.post('https://%s/ssl-vpn/hipreport.esp' % args.gateway, data=data)
+    del data['md5'] # official client doesn't resubmit MD5
+    r = s.post('https://%s/ssl-vpn/hipreport.esp' % args.gateway, data=data, verify=args.verify)
 
     if 'success' in r.text:
         print("HIP report submitted successfully.", file=stderr)
