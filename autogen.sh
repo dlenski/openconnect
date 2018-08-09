b48ce600be27547c68da410a998499c2b12308c6
Joerg Mayer <jmayer@loplof.de>
2018-08-09 15:38:06 +0200

    Check whether glibtoolize is available in addition to libtoolize
    macOS doesn't have libtoolize and GNU libtool only installs glibtoolize.
    
    Signed-off-by: Joerg Mayer <jmayer@loplof.de>
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+$(type glibtoolize >/dev/null 2>&1) && LIBTOOLIZE=${LIBTOOLIZE:-glibtoolize}
 aclocal -I m4 && \
 ${LIBTOOLIZE:-libtoolize} --automake --force && \
 autoheader --force &&
