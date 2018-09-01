0b38d645f1fdafda2cf584923a833decb81b02c8
Daniel Lenski <dlenski@gmail.com>
2018-08-31 17:26:58 -0700

    convert <jnlp> output to openconnect command-line

--- test-globalprotect-login.py
+++ test-globalprotect-login.py
@@ -2,10 +2,12 @@
 
 from __future__ import print_function
 from urlparse import urlparse
+from urllib import urlencode
 import requests
 import argparse
 import getpass
 import os
+import xml.etree.ElementTree as ET
 from sys import stderr
 
 p = argparse.ArgumentParser()
@@ -16,6 +18,7 @@ g = p.add_argument_group('Login credentials')
 g.add_argument('-u','--user', help='Username (will prompt if unspecified)')
 g.add_argument('-p','--password', help='Password (will prompt if unspecified)')
 g.add_argument('-c','--cert', help='PEM file containing client certificate (and optionally private key)')
+g.add_argument('--computer', default=os.uname()[1], help="Computer name (default is `hostname`)")
 g.add_argument('--key', help='PEM file containing client private key (if not included in same file as certificate)')
 g.add_argument('--no-verify', dest='verify', action='store_false', default=True, help='Ignore invalid server certificate')
 args = p.parse_args()
@@ -51,11 +54,31 @@ res = s.post(endpoint.geturl(), verify=args.verify,
                        jnlpReady='jnlpReady', ok='Login', direct='yes',
                        # optional but might affect behavior
                        clientVer=4100, server=endpoint.netloc, prot='https:',
-                       computer=os.uname()[1],
+                       computer=args.computer,
                        **extra))
 
 if args.verbose:
     print("Request body:\n", res.request.content, file=stderr)
 
 res.raise_for_status()
+print(res.headers)
 print(res.text)
+
+# build openconnect "cookie" if the result is a <jnlp>
+
+try:
+    xml = ET.fromstring(res.text)
+except Exception:
+    xml = None
+
+if xml.tag == 'jnlp':
+    arguments = [(t.text or '') for t in xml.iter('argument')]
+    cookie = urlencode({'authcookie': arguments[1], 'portal': arguments[3], 'user': arguments[4], 'domain': arguments[7],
+                        'computer': args.computer, 'preferred-ip': arguments[15] if len(arguments)>=16 else ''})
+    print('''
+
+Extracted connection cookie from <jnlp>. Use this to connect:
+
+    openconnect --protocol=gp --usergroup=gateway %s \\
+        --cookie "%s"
+''' % (endpoint.netloc, cookie))
