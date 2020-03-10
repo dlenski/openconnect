36eccd458b7e7a781588b0db0902fb56c74006a2
Daniel Lenski <dlenski@gmail.com>
2020-03-10 13:51:50 -0700

    DO NOT BUILD AND RUN THIS — THIS REPOSITORY REMAINS FOR HISTORICAL ISSUE TRACKING ONLY

--- autogen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-$(type glibtoolize >/dev/null 2>&1) && LIBTOOLIZE=${LIBTOOLIZE:-glibtoolize}
-aclocal -I m4 && \
-${LIBTOOLIZE:-libtoolize} --automake --force && \
-autoheader --force &&
-automake --foreign --add-missing --force && \
-autoconf --force
